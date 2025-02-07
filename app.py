ca2ebf8c88f407fe62f12ffe941a1cfa4763e446
DrOksusu <ok4192ok@gmail.com>
2025-02-07 13:34:19 +0900

    컨테이너포트 8000번 수정

--- app.py
+++ app.py
@@ -627,4 +627,4 @@ def create_ppt():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
